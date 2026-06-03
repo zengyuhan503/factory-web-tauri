c3c1fbe21ae1a4855177255ec1b044b7c374e040
zengyuhan <zengyuhan@skyworthxr.com>
2026-06-03 16:57:48 +0800

    chore(sfr_runner): 硬编码Python解释器路径为C:\py312\python.exe

--- src-tauri/src/adapters/sfr_runner.rs
+++ src-tauri/src/adapters/sfr_runner.rs
@@ -200,7 +200,7 @@ impl SfrRunner {
         let mean_str = format!("{}", mean_avg50_min);
         let std_str = format!("{}", cam_std_max);
 
-        let mut child = Command::new("python")
+        let mut child = Command::new("C:\\py312\\python.exe")
             .arg(&run_sfr_path)
             .arg(image_dir_str)
             .arg("--pattern-hint")
